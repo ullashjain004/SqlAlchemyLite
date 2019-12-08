8510882079321e812076f0c014e50131083c000c
Sneha Philip <snehaphilip989@gmail.com>
2019-12-07 19:26:57 -0500

    Feature: Updated python file imports and necessary changes with inputs

--- Database/sqlalchem_test.py
+++ Database/sqlalchem_test.py
@@ -1,13 +1,11 @@
 from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
 from sqlalchemy.orm import sessionmaker, Session, relationship
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import desc, func, cast, Date, distinct, union, DateTime, text, join, update
 from sqlalchemy import or_, and_, not_
 from datetime import datetime
-from sqlalchemy import desc, func, cast, Date, distinct, union, DateTime, text, join, update
-
-
+from sqlalchemy.exc import IntegrityError
 
 engine = create_engine('sqlite:////web/Sqlite-Data/example5.db')
 
@@ -29,25 +27,6 @@ class Customer(Base):
     orders = relationship("Order", backref='customer')
 
 
-class OrderLine(Base):
-    __tablename__ = 'order_lines'
-    id = Column(Integer(), primary_key=True)
-    order_id = Column(Integer(), ForeignKey('orders.id'))
-    item_id = Column(Integer(), ForeignKey('items.id'))
-    quantity = Column(Integer())
-    order = relationship("Order", backref='order_lines')
-    item = relationship("Item")
-
-
-
-
-class Order(Base):
-    __tablename__ = 'orders'
-    id = Column(Integer(), primary_key=True)
-    customer_id = Column(Integer(), ForeignKey('customers.id'))
-    date_placed = Column(DateTime(), default=datetime.now, nullable=False)
-    date_shipped = Column(DateTime())
-
 class Item(Base):
     __tablename__ = 'items'
     id = Column(Integer(), primary_key=True)
@@ -57,8 +36,25 @@ class Item(Base):
     quantity = Column(Integer(), nullable=False)
 
 
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    date_placed = Column(DateTime(), default=datetime.now, nullable=False)
+    date_shipped = Column(DateTime())
+
+
+class OrderLine(Base):
+    __tablename__ = 'order_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(Integer())
+    order = relationship("Order", backref='order_lines')
+    item = relationship("Item")
+
+# function to check valid order_id
 def dispatch_order(order_id):
-    # check whether order_id is valid or not
     order = session.query(Order).get(order_id)
 
     if not order:
@@ -81,6 +77,7 @@ def dispatch_order(order_id):
         print("Rolling back ...")
         session.rollback()
         print("Transaction failed.")
+
 
 Base.metadata.create_all(engine)
 
@@ -184,78 +181,132 @@ session.commit()
 for ol in c1.orders[0].order_lines:
     ol.id, ol.item, ol.quantity
 
-print('-------')
+#print('-------')
 
 for ol in c1.orders[1].order_lines:
     ol.id, ol.item, ol.quantity
 
-session.query(Item).filter(Item.name.ilike("wa%")).all()
-session.query(Item).filter(Item.name.ilike("wa%")).order_by(Item.cost_price).all()
+# Items that start with 'wa'
+output = session.query(Item).filter(Item.name.ilike("wa%")).all()
+print(" Items that start with wa :")
+for i in output:
+    print("Item Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
 
-# descending order
-session.query(Item).filter(Item.name.ilike("wa%")).order_by(desc(Item.cost_price)).all()
-
-session.query(Customer).join(Order).all()
-
-# print(session.query(Customer).join(Order))
+# Items that start with 'wa', sorted in descending order of price
+output = session.query(Item).filter(Item.name.ilike("wa%")).order_by(desc(Item.cost_price)).all()
+print(" Items that start with wa sorted in descending order of price:")
+for i in output:
+    print("Item Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
 
 session.query(Customer.id, Customer.username, Order.id).join(Order).all()
 
-# find all customers who either live in Peterbrugh or Norfolk
+# Find all customers who either live in Peterbrugh or Norfolk
+output = session.query(Customer).filter(or_(Customer.town == 'Peterbrugh', Customer.town == 'Norfolk')).all()
+print("Find all customers who either live in either Peterburgh or Norfolk")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
-session.query(Customer).filter(or_(
-    Customer.town == 'Peterbrugh',
-    Customer.town == 'Norfolk'
-)).all()
+# Find all customers whose first name is John and live in Norfolk
+output = session.query(Customer).filter(and_(Customer.first_name == 'John',Customer.town == 'Norfolk')).all()
+print("Find all customers whose first name is John and live in Norfolk")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
-# find all customers whose first name is John and live in Norfolk
+# Find all johns who don't live in Peterbrugh
+output = session.query(Customer).filter(and_(Customer.first_name == 'John',not_(Customer.town == 'Peterbrugh',))).all()
+print("Find all johns who don't live in Peterbrugh")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
-session.query(Customer).filter(and_(
-    Customer.first_name == 'John',
-    Customer.town == 'Norfolk'
-)).all()
+#Null shipping date
+output = session.query(Order).filter(Order.date_shipped == None).all()
+print("Find all orders with Null shipping date")
+for i in output:
+    print("ID: ", i.id, " Date Placed:", i.date_placed, " Customer Id:", i.customer_id)
 
-# find all johns who don't live in Peterbrugh
+#Not Null shipping date
+output = session.query(Order).filter(Order.date_shipped != None).all()
+print("Find all orders with not Null shipping date")
+for i in output:
+    print("ID: ", i.id, " Date Placed:", i.date_placed, " Customer Id:", i.customer_id)
 
-session.query(Customer).filter(and_(
-    Customer.first_name == 'John',
-    not_(
-        Customer.town == 'Peterbrugh',
-    )
-)).all()
+#Customers with first name as Toby or Sarah
+output = session.query(Customer).filter(Customer.first_name.in_(['Toby', 'Sarah'])).all()
+print("Find all Customers with first name as Toby or Sarah")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
-session.query(Order).filter(Order.date_shipped == None).all()
-session.query(Order).filter(Order.date_shipped != None).all()
-session.query(Customer).filter(Customer.first_name.in_(['Toby', 'Sarah'])).all()
-session.query(Customer).filter(Customer.first_name.notin_(['Toby', 'Sarah'])).all()
-session.query(Item).filter(Item.cost_price.between(10, 50)).all()
-session.query(Item).filter(not_(Item.cost_price.between(10, 50))).all()
-session.query(Item).filter(Item.name.like("%r")).all()
-session.query(Item).filter(Item.name.ilike("w%")).all()
-session.query(Item).filter(not_(Item.name.like("W%"))).all()
-session.query(Customer).limit(2).all()
-session.query(Customer).filter(Customer.address.ilike("%avenue")).limit(2).all()
+#Customers with first name not Toby or Sarah
+output = session.query(Customer).filter(Customer.first_name.notin_(['Toby', 'Sarah'])).all()
+print("Find all Customers with first name not Toby or Sarah")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
+
+#Item cost price between 10 and 50
+output = session.query(Item).filter(Item.cost_price.between(10, 50)).all()
+print("Find all Item cost price between 10 and 50")
+for i in output:
+    print("Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
+
+#Item cost price not between 10 and 50
+output = session.query(Item).filter(not_(Item.cost_price.between(10, 50))).all()
+print("Find all Item cost price not between 10 and 50")
+for i in output:
+    print("Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
+
+#Items that ends with a 'r'
+output = session.query(Item).filter(Item.name.like("%r")).all()
+print("Items that ends with a 'r'")
+for i in output:
+    print("Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
+
+#Items that begin with a 'w'
+output = session.query(Item).filter(Item.name.ilike("w%")).all()
+print("Items that begin  with a 'q'")
+for i in output:
+    print("Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
+
+#Items that begin with a 'W'
+output = session.query(Item).filter(not_(Item.name.like("W%"))).all()
+print("Items that begin with a 'W'")
+for i in output:
+    print("Name: ", i.name, " Cost Price:", i.cost_price, " Selling Price:", i.selling_price, " Quantity:",
+          i.quantity)
+
+#Print customer details but limit is 2
+output = session.query(Customer).limit(2).all()
+print("Print customer details but limit is 2")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
+
+#Print customer details where adress begins with avenue but only 2 entries
+output = session.query(Customer).filter(Customer.address.ilike("%avenue")).limit(2).all()
+print("Print customer details where adress begins with avenue but only 2 entries")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
 # find the number of customers lives in each town
+output = session.query(Customer).filter(Customer.address.ilike("%avenue")).limit(2).all()
+print("Print the number of customers lives in each town")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
-session.query(
-    func.count("*").label('town_count'),
-    Customer.town
-).group_by(Customer.town).having(func.count("*") > 2).all()
 
-session.query(Customer.town).filter(Customer.id < 10).all()
-session.query(Customer.town).filter(Customer.id < 10).distinct().all()
-
-session.query(
-    func.count(distinct(Customer.town)),
-    func.count(Customer.town)
-).all()
-
+session.query(func.count(distinct(Customer.town)),func.count(Customer.town)).all()
+#S1 : filter names that begin with Wa || S2: filter names that have an e
 s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
 s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
 s1.union(s2).all()
+#Find names that have both S1 and S2
+print("Union query")
+print (s1.union(s2).all())
 
-s1.union_all(s2).all()
 
 i = session.query(Item).get(8)
 i.selling_price = 25.91
@@ -263,18 +314,27 @@ session.add(i)
 session.commit()
 
 # update quantity of all quantity of items to 60 whose name starts with 'W'
-
-session.query(Item).filter(
-    Item.name.ilike("W%")
-).update({"quantity": 60}, synchronize_session='fetch')
+output = session.query(Item).filter(Item.name.ilike("W%")).update({"quantity": 60}, synchronize_session='fetch')
+print(output)
 session.commit()
 
-session.query(Customer).filter(text("first_name = 'John'")).all()
+#Customers with first name John
+output = session.query(Customer).filter(text("first_name = 'John'")).all()
+print("Find all Customers with first name John")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
+#Customers with town starting with Nor
 session.query(Customer).filter(text("town like 'Nor%'")).all()
+print("Find all Customers with town starting with Nor")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 
+#Customers with town starting with Nor ordered by first name
 session.query(Customer).filter(text("town like 'Nor%'")).order_by(text("first_name, id desc")).all()
-
+print("Find all Customers with town starting with Nor ordered by first name")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
 session.commit()
 
 dispatch_order(1)
